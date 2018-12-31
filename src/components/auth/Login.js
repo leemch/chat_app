d69bac40e4a2dd92c7e95950dacee3b94de34e11
leemch <lee7mchale@gmail.com>
2018-12-31 01:10:15 -0800

    added login and Loading

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,102 @@
 import React from "react";
+import {Grid, Form, Segment, Button, Header, Message, Icon} from "semantic-ui-react";
+import {Link} from "react-router-dom";
+import firebase from "../../firebase";
 
 class Login extends React.Component {
+    state = {
+        email: "",
+        password: "",
+        errors: [],
+        loading: false,
+        usersRef: firebase.database().ref("users")
+    };
+
+
+    isPasswordValid = ({password, password2}) => {
+        if(password.length < 6 || password2.length < 6) {
+            return false;
+        } else if(password !== password2){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>);
+
+   
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (this.isFormValid(this.state)) {
+            this.setState({errors: [], loading: true})
+            firebase.auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(signedInUser => {
+                console.log(signedInUser);
+            })
+            .catch(err => {
+                console.error(err);
+                this.setState({
+                    errors: this.state.errors.concat(err),
+                    loading: false
+                });
+            });
+        }
+    }
+
+    isFormValid = ({email, password}) => {
+        return email && password;
+    }
+
+    handleInputError = (errors, inputName) => {
+        return errors.some(error => error.message.toLowerCase().includes(inputName)) ? "error" : "";
+    }
+
     render() {
+
+        const { email, password, errors, loading} = this.state;
+
         return(
-            <div>Login</div>
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+                <Grid.Column style={{maxWidth: 450}}>
+                    <Header as="h1" icon color="violet" textAlign="center">
+                        <Icon name="code branch" color="violet" />
+                        Log in to Meme wars
+                    </Header>
+                    <Form onSubmit={this.handleSubmit} size="large">
+                        <Segment stacked>
+
+
+                            <Form.Input 
+                            className={this.handleInputError(errors, 'email')}
+                            value={email} fluid name="email" icon="mail" iconPosition="left"
+                            placeholder="Email Address" onChange={this.handleChange} type='email'/>
+
+                            <Form.Input 
+                            className={this.handleInputError(errors, 'password')}
+                            value={password} fluid name="password" icon="lock" iconPosition="left"
+                            placeholder="Password" onChange={this.handleChange} type='password'/>
+
+                            <Button disabled={loading} className={loading ? "loading" : ""} color="violet" fluid size="large">Submit</Button>
+                            <Message>Don't have an account? <Link to="/register">Register</Link></Message>
+                        </Segment>
+                    </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    )}
+                </Grid.Column>
+            </Grid>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,22 +3,64 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, withRouter} from "react-router-dom";
+
+import {createStore} from "redux";
+import {Provider, connect} from "react-redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import {setUser} from "./actions";
+
+
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Spinner from "./Spinner";
+import firebase from "./firebase";
 
 import 'semantic-ui-css/semantic.min.css'
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-);
+const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+class Root extends React.Component {
+
+
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user=> {
+            if(user){
+                this.props.setUser(user);
+                this.props.history.push('/');
+            }
+        })
+    }
+
+    render() {
+
+        return this.props.isLoading ? <Spinner /> : (
+
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+
+        );
+
+    }
+
+}
+
+    const mapStateToProps = state => ({
+       isLoading: state.user.isLoading
+    });
+
+    const RootWithAuth = withRouter(connect(mapStateToProps, {setUser})(Root));
+
+ReactDOM.render(
+<Provider store={store}>
+<Router>
+<RootWithAuth />
+</Router>
+</Provider>,
+ document.getElementById('root'));
 
 serviceWorker.unregister();
